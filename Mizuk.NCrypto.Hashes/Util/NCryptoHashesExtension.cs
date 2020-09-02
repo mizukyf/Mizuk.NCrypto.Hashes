24e9d8cb86ff77f195a7921eb82f7d85d48504d0
mizuky fujitani <mizuki.fujitani@gmail.com>
2020-09-02 09:52:43 +0900

    拡張メソッド提供クラスにコメントを付与

--- Mizuk.NCrypto.Hashes/Util/NCryptoHashesExtension.cs
+++ Mizuk.NCrypto.Hashes/Util/NCryptoHashesExtension.cs
@@ -5,13 +5,27 @@ namespace Mizuk.NCrypto.Hashes.Util
     /// <summary>
     /// This code is derived from  RustCrypto/hashes and Rust std modules.
     /// Ported by mizuky at 2020/09/01.
+    /// 
+    /// Mizuk.NCrypto.Hashes内のコードのため、RustからC#へのポーティングをできるだけシンプルに行うための
+    /// 各種の拡張メソッドを提供するユーティリティクラスです。
     /// </summary>
     static class NCryptoHashesExtension
     {
-        public static uint RotateLeft(this uint v, int n)
+        /// <summary>
+        /// ビットを左方向にシフト、ローテートを行います。
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public static uint RotateLeft(this uint value, int n)
         {
-            return (v << n) | (v >> (32 - n));
+            return (value << n) | (value >> (32 - n));
         }
+        /// <summary>
+        /// 4要素からなるバイト配列を元にリトルエンディアンで整数値に変換します。
+        /// </summary>
+        /// <param name="bytes"></param>
+        /// <returns></returns>
         public static uint FromLittleEndianBytes(this byte[] bytes)
         {
             if (bytes.Length != 4) throw new ArgumentException("bytes' length must be 4.");
@@ -24,6 +38,11 @@ namespace Mizuk.NCrypto.Hashes.Util
             }
             return BitConverter.ToUInt32(bytes, 0);
         }
+        /// <summary>
+        /// 整数値をリトルエンディアンのバイト配列に変換します。
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
         public static byte[] ToLittleEndianBytes(this ulong value)
         {
             var bytes = BitConverter.GetBytes(value);
@@ -33,6 +52,11 @@ namespace Mizuk.NCrypto.Hashes.Util
             }
             return bytes;
         }
+        /// <summary>
+        /// 整数値をリトルエンディアンのバイト配列に変換します。
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
         public static byte[] ToLittleEndianBytes(this uint value)
         {
             var bytes = BitConverter.GetBytes(value);
@@ -42,6 +66,12 @@ namespace Mizuk.NCrypto.Hashes.Util
             }
             return bytes;
         }
+        /// <summary>
+        /// バイト配列を指定されたチャンクサイズに対応する<see cref="ChunksExact"/>インスタンスを返します。
+        /// </summary>
+        /// <param name="values"></param>
+        /// <param name="chunkSize"></param>
+        /// <returns></returns>
         public static ChunksExact ChunksExact(this byte[] values, int chunkSize)
         {
             return new ChunksExact(values, chunkSize);
