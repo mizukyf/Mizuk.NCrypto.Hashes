248a314680fd08661062938e58ec818903d72258
mizuky fujitani <mizuki.fujitani@gmail.com>
2020-09-03 22:56:24 +0900

    ICloneのコメントを追加

--- Mizuk.NCrypto.Hashes/Traits/IClone.cs
+++ Mizuk.NCrypto.Hashes/Traits/IClone.cs
@@ -1,7 +1,15 @@
 ﻿namespace Mizuk.NCrypto.Hashes.Traits
 {
+    /// <summary>
+    /// オブジェクトの明示的なコピーの機能を提供するインターフェースです。
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
     public interface IClone<T>
     {
+        /// <summary>
+        /// オブジェクトをクローンします。
+        /// </summary>
+        /// <returns></returns>
         T Clone();
     }
 }
