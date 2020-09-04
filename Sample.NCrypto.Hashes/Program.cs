b2c7953178ccdb19782eff950661327dad9d6c62
mizuky fujitani <mizuki.fujitani@gmail.com>
2020-09-04 21:28:10 +0900

    MD5のサンプルコードを追加

--- Sample.NCrypto.Hashes/Program.cs
+++ Sample.NCrypto.Hashes/Program.cs
@@ -11,13 +11,25 @@ namespace Sample.NCrypto.Hashes
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("original  : {0}", args[0]);
-            Console.WriteLine("ncrypto   : {0}", Md4.Digest(args[0]).ToHexString(hyphenSeparated: true));
+            Console.WriteLine("data  : {0}", args[0]);
+
+            var data = Encoding.Unicode.GetBytes(args[0]);
 
             var md4 = new SharpCifs.Util.Md4();
-            md4.Update(Encoding.Unicode.GetBytes(args[0]));
+            md4.Update(data);
 
+            Console.WriteLine();
+            Console.WriteLine("[MD4]");
+            Console.WriteLine("ncrypto   : {0}", Md4.Digest(data).ToHexString(hyphenSeparated: true));
             Console.WriteLine("sharpcifs : {0}", md4.Digest().ToHexString(hyphenSeparated: true));
+
+            Console.WriteLine();
+            Console.WriteLine("[MD5]");
+            
+            var md5 = new System.Security.Cryptography.MD5CryptoServiceProvider();
+            Console.WriteLine("ncrypto   : {0}", Md5.Digest(data).ToHexString(hyphenSeparated: true));
+            Console.WriteLine("standard  : {0}", md5.ComputeHash(data).ToHexString(hyphenSeparated: true));
+
         }
     }
 }
