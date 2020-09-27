9c267acef6f25c9cbe23a3912ff833b63567c85a
mizuky fujitani <mizuki.fujitani@gmail.com>
2020-09-27 17:54:41 +0900

    簡易のテストコードを追加

---
+++ Test.NCrypto.Hashes/Md4Test.cs
@@ -0,0 +1,56 @@
+﻿using NCrypto.Hashes;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test.NCrypto.Hashes
+{
+
+    [TestFixture]
+    public class Md4Test
+    {
+        [TestCase("a")]
+        [TestCase("ab")]
+        [TestCase("abc")]
+        [TestCase("0")]
+        [TestCase("01")]
+        [TestCase("012")]
+        [TestCase("@")]
+        [TestCase("@*")]
+        [TestCase("@*!")]
+        [TestCase("あ")]
+        [TestCase("あい")]
+        [TestCase("あいう")]
+        public void CompareWithSharpCifsImpl(string src)
+        {
+            var data = Encoding.Unicode.GetBytes(src);
+
+            var md4 = new SharpCifs.Util.Md4();
+            md4.Update(data);
+
+            Assert.That(Md4.Digest(data), Is.EqualTo(md4.Digest()));
+        }
+        [TestCase('x', 0)]
+        [TestCase('x', 10)]
+        [TestCase('x', 100)]
+        [TestCase('x', 1000)]
+        [TestCase('ｘ', 10)]
+        [TestCase('ｘ', 100)]
+        [TestCase('ｘ', 1000)]
+        [TestCase('ん', 10)]
+        [TestCase('ん', 100)]
+        [TestCase('ん', 1000)]
+        public void CompareWithSharpCifsImpl(char ch, int len)
+        {
+            var data = Encoding.Unicode.GetBytes(Enumerable.Repeat(ch, len).ToArray());
+
+            var md4 = new SharpCifs.Util.Md4();
+            md4.Update(data);
+
+            Assert.That(Md4.Digest(data), Is.EqualTo(md4.Digest()));
+        }
+    }
+}
---
+++ Test.NCrypto.Hashes/Md5Test.cs
@@ -0,0 +1,57 @@
+﻿using NCrypto.Hashes;
+using NUnit.Framework;
+using System;
+using System.Linq;
+using System.Text;
+
+namespace Test.NCrypto.Hashes
+{
+    [TestFixture]
+    public class Md5Test
+    {
+        [TestCase("a")]
+        [TestCase("ab")]
+        [TestCase("abc")]
+        [TestCase("0")]
+        [TestCase("01")]
+        [TestCase("012")]
+        [TestCase("@")]
+        [TestCase("@*")]
+        [TestCase("@*!")]
+        [TestCase("あ")]
+        [TestCase("あい")]
+        [TestCase("あいう")]
+        public void CompareWithStandardImpl(string src)
+        {
+            var data = Encoding.Unicode.GetBytes(src);
+
+            var standardMd5 = new System.Security.Cryptography.MD5CryptoServiceProvider().ComputeHash(data);
+            var ncryptMd5 = Md5.Digest(data);
+
+            Assert.That(standardMd5, Is.EqualTo(ncryptMd5));
+            Assert.That(BitConverter.ToString(standardMd5), Is.EqualTo(ncryptMd5.ToHexString(hyphenSeparated: true)));
+            Assert.That(BitConverter.ToString(standardMd5).Replace("-", string.Empty), Is.EqualTo(ncryptMd5.ToHexString(hyphenSeparated: false)));
+        }
+        [TestCase('x', 0)]
+        [TestCase('x', 10)]
+        [TestCase('x', 100)]
+        [TestCase('x', 1000)]
+        [TestCase('ｘ', 10)]
+        [TestCase('ｘ', 100)]
+        [TestCase('ｘ', 1000)]
+        [TestCase('ん', 10)]
+        [TestCase('ん', 100)]
+        [TestCase('ん', 1000)]
+        public void CompareWithStandardImpl(char ch, int len)
+        {
+            var data = Encoding.Unicode.GetBytes(Enumerable.Repeat(ch, len).ToArray());
+
+            var standardMd5 = new System.Security.Cryptography.MD5CryptoServiceProvider().ComputeHash(data);
+            var ncryptMd5 = Md5.Digest(data);
+
+            Assert.That(standardMd5, Is.EqualTo(ncryptMd5));
+            Assert.That(BitConverter.ToString(standardMd5), Is.EqualTo(ncryptMd5.ToHexString(hyphenSeparated: true)));
+            Assert.That(BitConverter.ToString(standardMd5).Replace("-", string.Empty), Is.EqualTo(ncryptMd5.ToHexString(hyphenSeparated: false)));
+        }
+    }
+}
